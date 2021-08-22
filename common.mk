802708fbd9a7a243eb12065cd0f5f62763eafdfc
Harshit Jain <harshitjain6751@gmail.com>
2021-08-22 16:36:13 +0800

    msm8998-common: Strip off some debug packages
    * Ref: https://source.android.com/devices/tech/dalvik/configure#compilation_options
    
    Q: PRODUCT_MINIMIZE_JAVA_DEBUG_INFO wth does this do?
    A: https://android.googlesource.com/platform/build/+/master/target/product/go_defaults_common.mk#68
    
    Change-Id: Iad073ec85f5cfe5be5a8d1944dd7cff521077dbe

--- common.mk
+++ common.mk
@@ -386,9 +386,12 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
 
 # Telephony
 PRODUCT_PACKAGES += \
