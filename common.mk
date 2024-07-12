69772f434d3352eb7139d24db151bf4f11d883ad
NurKeinNeid <mralexman3000@gmail.com>
2024-07-12 10:27:49 -0300

    msm8998-common: Adjust common for derp
    Change-Id: Ia83e33117ee7f627d3e67b3969469ccf8e9732d6

--- common.mk
+++ common.mk
@@ -14,7 +14,7 @@ PRODUCT_COMPRESSED_APEX := false
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-derp
 
 PRODUCT_PACKAGES += \
      CarrierConfigOverlay \
